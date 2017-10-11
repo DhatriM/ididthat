ee15166c583531fff3f2de97333d21c920593e12
rusathe <riahsathe94@gmail.com>
2017-10-10 21:22:35 -0700

    Json and js files for places

---
+++ src/resources/places.js
@@ -0,0 +1,26 @@
+db = connect("localhost:8080/ididthat");
+coll = db.places;
+coll.insert({   "id" : "156",
+    "PlaceName" : "Place1",
+    "PlaceLat" : 37.4,
+    "PlaceLong" : -122.0,
+    "PlaceCategoryType" : "Restaurant",
+    "numberCheckins" : 0,
+    "avgRating" : 2.5,
+    "latestRankingbyCategory" : 1});
+coll.insert({    "id" : "158",
+    "PlaceName" : "Place2",
+    "PlaceLat" : 39.4,
+    "PlaceLong" : -122.0,
+    "PlaceCategoryType" : "Restaurant",
+    "numberCheckins" : 0,
+    "avgRating" : 2.9,
+    "latestRankingbyCategory" : 2});
+coll.insert({    "id" : "162",
+    "PlaceName" : "Place3",
+    "PlaceLat" : 37.4,
+    "PlaceLong" : -124.0,
+    "PlaceCategoryType" : "Park",
+    "numberCheckins" : 1,
+    "avgRating" : 3.5,
+    "latestRankingbyCategory" : 3});
